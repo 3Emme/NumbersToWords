10f2c6d3da055261d0f2809b24f747fb28ac235c
3Emme <emmettbuentiempo@gmail.com>
2020-09-24 10:50:26 -0700

    write initial structure for NumberParser.
    co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- NumbersToWords/Models/NumbersToWords.cs
+++ NumbersToWords/Models/NumbersToWords.cs
@@ -4,6 +4,17 @@ namespace NumbersToWords
 {
   public class Translator
   {
+    public string NumberString { get; set; }
+    public void WordNumber(string numberString)
+    {
+      NumberString = numberString;
+    }
 
+    public string NumberParser()
+    {
+      string result;
+      result = "I have no idea what you mean.";
+      return result;
+    }
   }
 }

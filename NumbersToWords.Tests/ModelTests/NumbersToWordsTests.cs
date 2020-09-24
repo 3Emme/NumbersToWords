ab21bd3a94e227ee6ed294518d8376343698b36f
3Emme <emmettbuentiempo@gmail.com>
2020-09-24 10:15:15 -0700

    create initial file strucutre for program and tests.
    co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

---
+++ NumbersToWords.Tests/ModelTests/NumbersToWordsTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumbersToWords;
+
+namespace NumbersToWords.Tests
+{
+  [TestClass]
+  public class NumbersToWordsTests
+  {
+    [TestMethod]
+    public void blah()
+    {
+      //testing code will go here
+    }
+  }
+}
---
+++ NumbersToWords/Models/NumbersToWords.cs
@@ -0,0 +1,7 @@
+namespace NumbersToWords
+{
+  public class Translator
+  {
+    
+  }
+}

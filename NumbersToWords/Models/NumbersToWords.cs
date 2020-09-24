ff9ed18f33d05b41f295d74471a25a756d709f4f
3Emme <emmettbuentiempo@gmail.com>
2020-09-24 11:18:04 -0700

    refactor NumberParser to pass test 1.
    co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- NumbersToWords/Models/NumbersToWords.cs
+++ NumbersToWords/Models/NumbersToWords.cs
@@ -1,19 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 namespace NumbersToWords
 {
   public class Translator
   {
-    public string NumberString { get; set; }
-    public void WordNumber(string numberString)
-    {
-      NumberString = numberString;
-    }
+    // public string NumberString { get; set; }
+    // public void Translator(string numberString)
+    // {
+    //   NumberString = numberString;
+    // }
 
-    public string NumberParser()
+    static public string NumberParser(string numberString)
     {
-      string result;
-      result = "I have no idea what you mean.";
+      string result;      
+      // Dictionary<string, string> singlesDict = new Dictionary<string, string>() { {"1","one"} };
+      // char[] numberArray = numberString.ToCharArray();
+      // if (numberArray.length = 1)
+      // {
+      //   if
+      // }
+      if (numberString == "1")
+      {
+        result = "one";
+      }
+      else
+      {
+        result = "I have no idea what you mean.";
+      }
       return result;
     }
   }
